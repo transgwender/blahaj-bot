b0f9df17fbee14fd8ccc70efd95b61fe60167f32
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-05-01 18:20:44 +0530

    Setup basic bot

--- blahaj_bot/__init__.py
+++ blahaj_bot/__init__.py
@@ -1,2 +1,16 @@
+from blahaj_bot.client import MyClient
+import discord
+import os
+from dotenv import load_dotenv
+
 def bot() -> None:
     print("Hello from blahaj-bot!!!")
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+
+    client = MyClient(intents=intents)
+    client.run(TOKEN)

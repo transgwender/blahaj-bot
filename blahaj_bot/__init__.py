29ba0bb218ed05c1ed560b6618fe61fb4ab18e42
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-05-02 01:58:42 +0530

    added sys argv

--- blahaj_bot/__init__.py
+++ blahaj_bot/__init__.py
@@ -1,10 +1,11 @@
 from blahaj_bot.client import MyClient
 import discord
 import os
+import sys
 from dotenv import load_dotenv
 
 def bot() -> None:
-    print("Hello from blahaj-bot!!!")
+    print(sys.argv[1], "Hello from blahaj-bot!!!")
 
     intents = discord.Intents.default()
     intents.message_content = True

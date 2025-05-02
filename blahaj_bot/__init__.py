b43a07e503f018de113b1e9b1e4d7006783fbf97
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-05-02 15:58:02 +0530

    adjust logging

--- blahaj_bot/__init__.py
+++ blahaj_bot/__init__.py
@@ -7,9 +7,11 @@ logger = logging.getLogger(__name__)
 
 def bot() -> None:
 
-    logging.basicConfig(filename='blahaj.log', level=logging.INFO)
-
-    logger.info(sys.argv[1], "Hello from blahaj-bot!!!")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     intents = discord.Intents.default()
     intents.message_content = True

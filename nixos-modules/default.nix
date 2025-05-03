253ad62c160efa3b9d44115885270c444fce829f
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-05-04 02:32:44 +0530

    Updated to use flake-utils to compile for more systems

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -3,12 +3,4 @@
 rec {
   blahaj-bot = import ./blahaj-bot-service.nix;
   default = blahaj-bot;
-
-  overlayNixpkgsForThisInstance =
-    { pkgs, ... }:
-    {
-      nixpkgs = {
-        inherit overlays;
-      };
-    };
 }

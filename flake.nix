1ea597e5dca626e58cf724857398936a75fe2524
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-05-04 02:15:47 +0530

    shrunk nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Hello world flake using uv2nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
 
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";

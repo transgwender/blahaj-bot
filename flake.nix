a84ae8e19a4e2dfd1a790f5fdf420af7c9002e32
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-05-07 15:38:33 -0600

    adjust

--- flake.nix
+++ flake.nix
@@ -268,7 +268,7 @@
               age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ]; # isn't set automatically for some reason
                   
               age.secrets.blahaj-bot-token = {
-                file = ../secrets/blahaj-bot-token.age;
+                file = /secrets/blahaj-bot-token.age;
                 owner = "blahaj-bot";
               };
                   

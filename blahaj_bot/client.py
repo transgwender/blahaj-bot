25c251b5f809a8745c64e323536b9fe53e44436f
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-06-07 21:44:10 -0700

    Change to now use match pattern

--- blahaj_bot/client.py
+++ blahaj_bot/client.py
@@ -2,7 +2,7 @@ import discord
 from logging import Logger
 from typing import Any, Mapping
 from backloggery import Game, BacklogClient
-from discord import Intents
+from discord import Intents, Message
 from pymongo import MongoClient
 
 from blahaj_bot import __version__
@@ -23,29 +23,33 @@ class MyClient(discord.Client):
     async def on_ready(self):
         self.logger.info(f'Logged on as {self.user}! - Version {__version__}')
 
-    async def on_message(self, message):
+    async def on_message(self, message: Message):
         self.logger.info(f'Message from {message.author}: {message.content}')
 
-        if message.author == self.user:
+        if message.author == self.user or not message.content.startswith('$'):
             return
 
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
-        
-        if message.content.startswith('$pat'):
-            await message.channel.send('pat the pand')
+        incoming = message.content.replace('$', '').lower().strip().split()
+        command = incoming.pop(0)
 
-        if message.content.startswith('$github'):
-            await message.channel.send('Check out my source code at: https://github.com/transgwender/blahaj-bot')
-
-        if message.content.startswith('$version'):
-            await message.channel.send(f'Version {__version__}')
-
-        if message.content.startswith('$role'):
-            serverdb = self.db[str(message.guild.id)]
-            rolescol = serverdb["roles"]
-            result = rolescol.replace_one({"role" : "debug"}, { "role": "debug" }, upsert = True)
-            self.logger.info(f'{message.guild.name} -- {result}')
-            for x in rolescol.find():
-                self.logger.info(f'{x}')
-            await message.channel.send('WIP')
+        match command:
+            case "hello":
+                await message.channel.send('Hello!')
+            case "pat":
+                await message.channel.send('pat the pand')
+            case "github":
+                await message.channel.send('Check out my source code at: https://github.com/transgwender/blahaj-bot')
+            case "version":
+                await message.channel.send(f'Version {__version__}')
+            case "role":
+                serverdb = self.db[str(message.guild.id)]
+                rolescol = serverdb["roles"]
+                result = rolescol.replace_one({"role" : "debug"}, { "role": "debug" }, upsert = True)
+                self.logger.info(f'{message.guild.name} -- {result}')
+                for x in rolescol.find():
+                    self.logger.info(f'{x}')
+                await message.channel.send('WIP')
+            case "backloggery":
+                await message.channel.send('WIP')
+            case _:
+                await message.channel.send('Unknown command')

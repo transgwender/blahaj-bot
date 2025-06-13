fe59212bb60f07456f8acf638affea6b2f6e529c
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-06-13 00:44:56 -0700

    Test2

--- blahaj_bot/cogs/roles.py
+++ blahaj_bot/cogs/roles.py
@@ -9,28 +9,14 @@ from blahaj_bot import BotClient
 logger = logging.getLogger(__name__)
 
 class MyView(discord.ui.View):
-    @discord.ui.select(  # the decorator that lets you specify the properties of the select menu
-        placeholder="Choose a Flavor!",  # the placeholder text that will be displayed if nothing is selected
+    @discord.ui.role_select(
+        placeholder="Select a role!",  # the placeholder text that will be displayed if nothing is selected
         min_values=1,  # the minimum number of values that must be selected by the users
         max_values=1,  # the maximum number of values that can be selected by the users
-        options=[  # the list of options from which users can choose, a required field
-            discord.SelectOption(
-                label="Vanilla",
-                description="Pick this if you like vanilla!"
-            ),
-            discord.SelectOption(
-                label="Chocolate",
-                description="Pick this if you like chocolate!"
-            ),
-            discord.SelectOption(
-                label="Strawberry",
-                description="Pick this if you like strawberry!"
-            )
-        ]
     )
     async def select_callback(self, select,
-                              interaction):  # the function called when the user is done selecting options
-        await interaction.response.send_message(f"Awesome! I like {select.values[0]} too!")
+                              interaction):
+        await interaction.response.send_message(f"{select.values[0]}")
 
 class Roles(commands.Cog):
 

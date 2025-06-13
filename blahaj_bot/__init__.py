78762ad239b1de3c9446614339509b5b7f79d26a
Gwen Kornak <gwen@gwenkornak.ca>
2025-06-13 00:56:27 +0000

    Search (#7)
    * Add more search options
    * Dont display fields if empty
    * Made last updated not inline

--- blahaj_bot/__init__.py
+++ blahaj_bot/__init__.py
@@ -4,7 +4,7 @@ Discord Bot
 
 __title__ = 'blahaj-bot'
 __author__ = 'transgwender'
-__version__ = '0.0.11'
+__version__ = '0.0.12'
 
 import time
 import aiohttp
--- blahaj_bot/cogs/backlog.py
+++ blahaj_bot/cogs/backlog.py
@@ -3,7 +3,8 @@ import logging
 from datetime import datetime
 
 import discord
-from backloggery import Game, NoDataFoundError
+from backloggery import Game, NoDataFoundError, Priority, Region, PhysDigi
+from backloggery.enums import Status, Own
 from discord import SlashCommandGroup
 from discord.ext import commands, pages
 
@@ -20,24 +21,24 @@ def is_json(myjson):
 
 def create_game_embed(timestamp: datetime, game: Game):
     embed = discord.Embed(title=game.title, description="" if game.notes is None else game.notes)
-    if game.status is not None:
+    if game.status is not None and str(game.status) is not "":
         embed.add_field(name="Status", value=str(game.status))
-    if game.priority is not None:
+    if game.priority is not None and str(game.priority) is not "":
         embed.add_field(name="Priority", value=str(game.priority))
-    if game.platform_title is not None:
+    if game.platform_title is not None and str(game.platform_title) is not "":
         embed.add_field(name="Platform", value=game.platform_title)
-    if game.sub_platform_title is not None:
+    if game.sub_platform_title is not None and str(game.sub_platform_title) is not "":
         embed.add_field(name="Sub-Platform", value=game.sub_platform_title)
-    if game.region is not None:
+    if game.region is not None and str(game.region) is not "":
         embed.add_field(name="Region", value=str(game.region))
-    if game.phys_digi is not None:
+    if game.phys_digi is not None and str(game.phys_digi) is not "":
         embed.add_field(name="Format", value=str(game.phys_digi))
-    if game.own is not None:
+    if game.own is not None and str(game.own) is not "":
         embed.add_field(name="Ownership", value=str(game.own))
     if game.achieve_score is not None and game.achieve_total is not None and game.achieve_total > 0:
         embed.add_field(name="Achievements", value=f"{game.achieve_score}/{game.achieve_total}")
-    if game.last_update is not None:
-        embed.add_field(name="Last Updated", value=game.last_update)
+    if game.last_update is not None and str(game.last_update) is not "":
+        embed.add_field(name="Last Updated", value=game.last_update, inline=False)
     embed.set_footer(text=f'Last fetched: {timestamp.strftime("%Y-%m-%d %H:%M:%S")} - {timestamp.tzname()}')
 
     if game.has_review: # TODO: Review grab and embed
@@ -60,9 +61,23 @@ class Backlog(commands.Cog):
     @discord.option("username", description="Username", input_type=str)
     @discord.option("title", description="Title", input_type=str, required=False)
     @discord.option("abbr", description="Console abbreviation", input_type=str, required=False)
-    async def basic(self, ctx: discord.ApplicationContext, username, title, abbr):
-        search = json.dumps({"abbr": f'(?i)^.*{abbr}.*$' if abbr is not None else '',
-                             "title": f'(?i)^.*{title}.*$' if title is not None else ''})
+    @discord.option("platform", description="Platform", input_type=str, required=False)
+    @discord.option("status", description="Status", input_type=Status, required=False)
+    @discord.option("priority", description="Priority", input_type=Priority, required=False)
+    @discord.option("region", description="Region", input_type=Region, required=False)
+    @discord.option("format", description="Format", input_type=PhysDigi, required=False)
+    @discord.option("ownership", description="Ownership", input_type=Own, required=False)
+    async def basic(self, ctx: discord.ApplicationContext, username, title, abbr, status, priority, platform, region, format, ownership):
+        search = json.dumps({"title": f'(?i)^.*{title}.*$' if title is not None else '',
+                             "abbr": f'(?i)^.*{abbr}.*$' if abbr is not None else '',
+                             "platform_title": f'(?i)^.*{platform}.*$' if platform is not None else '',
+                             "status": f'(?i)^.*{str(status)}.*$' if status is not None else '',
+                             "priority": f'(?i)^.*{str(priority)}.*$' if priority is not None else '',
+                             "region": f'(?i)^.*{str(region)}.*$' if region is not None else '',
+                             "phys_digi": f'(?i)^.*{str(format)}.*$' if format is not None else '',
+                             "own": f'(?i)^.*{str(ownership)}.*$' if ownership is not None else '',})
+
+        logger.info(search)
 
         try:
             timestamp, result = self.bot.backlog.search_library(username=username, search_regex=search)

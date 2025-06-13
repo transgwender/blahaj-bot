41dfa70b41902d075f81cd3e8452917b67e33d98
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-06-13 00:36:29 -0700

    Testing

--- blahaj_bot/cogs/roles.py
+++ blahaj_bot/cogs/roles.py
@@ -1,12 +1,36 @@
 import logging
 
 import discord
-from discord import SlashCommandGroup
+from discord import SlashCommandGroup, MessageCommand
 from discord.ext import commands
 
 from blahaj_bot import BotClient
 
 logger = logging.getLogger(__name__)
+
+class MyView(discord.ui.View):
+    @discord.ui.select(  # the decorator that lets you specify the properties of the select menu
+        placeholder="Choose a Flavor!",  # the placeholder text that will be displayed if nothing is selected
+        min_values=1,  # the minimum number of values that must be selected by the users
+        max_values=1,  # the maximum number of values that can be selected by the users
+        options=[  # the list of options from which users can choose, a required field
+            discord.SelectOption(
+                label="Vanilla",
+                description="Pick this if you like vanilla!"
+            ),
+            discord.SelectOption(
+                label="Chocolate",
+                description="Pick this if you like chocolate!"
+            ),
+            discord.SelectOption(
+                label="Strawberry",
+                description="Pick this if you like strawberry!"
+            )
+        ]
+    )
+    async def select_callback(self, select,
+                              interaction):  # the function called when the user is done selecting options
+        await interaction.response.send_message(f"Awesome! I like {select.values[0]} too!")
 
 class Roles(commands.Cog):
 
@@ -24,6 +48,10 @@ class Roles(commands.Cog):
         for x in rolescol.find():
             logger.info(f'{x}')
         await ctx.respond('WIP')
+
+    @commands.message_command(name="Add Role-Reactions")
+    async def add_role_reaction(self, ctx: discord.ApplicationContext, message: discord.Message):
+        await ctx.respond("Choose a flavor!", view=MyView())
 
 def setup(bot):
     bot.add_cog(Roles(bot)) # add the cog to the bot

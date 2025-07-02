35d5f67101bf3e6d28ac57187866f19caf7a2049
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-07-01 21:50:50 -0700

    Added mapping

--- blahaj_bot/cogs/roles.py
+++ blahaj_bot/cogs/roles.py
@@ -25,7 +25,7 @@ class AssignableRole:
         return f'Emoji: {self.emoji} - Role Message ID: {self.role_msg_id} - Role ID: {self.role_id} - Server ID: {self.server_id}'
         
 assignable_roles: list[AssignableRole] = list()
-mappings: dict[int, dict[int, AssignableRole]] = defaultdict(dict[int, AssignableRole])
+mappings: dict[int, dict[int, dict[PartialEmoji, AssignableRole]]] = dict() # server id -> message id -> emoji -> role
 
 async def process_add_role(role: Role, emoji: Emoji|str, msg: Message, interaction: Interaction):
     await interaction.followup.edit_message(interaction.message.id, content=f"Added emoji: {emoji} for {role}.", view=None)
@@ -33,7 +33,13 @@ async def process_add_role(role: Role, emoji: Emoji|str, msg: Message, interacti
     ar = AssignableRole(msg.guild.id, role.id, emoji, msg.id)
     logger.info(f'Add Assignable Role: {ar}')
     assignable_roles.append(ar)
-    mappings[ar.server_id][ar.role_msg_id] = ar
+
+    if ar.server_id not in mappings:
+        mappings[ar.server_id] = dict()
+    if ar.role_msg_id not in mappings[ar.server_id]:
+        mappings[ar.server_id][ar.role_msg_id] = dict()
+
+    mappings[ar.server_id][ar.role_msg_id][ar.emoji] = ar
     logger.info(f'{mappings}')
 
 class AddRoleView(discord.ui.View):
@@ -95,10 +101,73 @@ class Roles(commands.Cog):
         """Gives a role based on a reaction emoji."""
         logger.info(f'Reaction Added: {payload.emoji} to {payload.message_id} by {payload.user_id}')
 
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is None or payload.guild_id not in mappings:
+            # Make sure we're still in the guild, and it's cached.
+            return
+
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id not in mappings[payload.guild_id]:
+            return
+
+        # If the emoji isn't the one we care about then exit as well.
+        if payload.emoji not in mappings[payload.guild_id][payload.message_id]:
+            return
+        
+        role_id = mappings[payload.guild_id][payload.message_id][payload.emoji].role_id
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        try:
+            # Finally, add the role.
+            await payload.member.add_roles(role)
+            logger.info(f'Added role {role} to {payload.member}')
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+
     @commands.Cog.listener("on_raw_reaction_remove")
     async def process_reaction_remove(self, payload: discord.RawReactionActionEvent):
         """Removes a role based on a reaction emoji."""
         logger.info(f'Reaction Removed: {payload.emoji} from {payload.message_id} by {payload.user_id}')
+
+        # Make sure we're still in the guild, and it's cached.
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is None or payload.guild_id not in mappings:
+            return
+
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id not in mappings[payload.guild_id]:
+            return
+
+        # If the emoji isn't the one we care about then exit as well.
+        if payload.emoji not in mappings[payload.guild_id][payload.message_id]:
+            return
+        
+        role_id = mappings[payload.guild_id][payload.message_id][payload.emoji].role_id
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        # The payload for `on_raw_reaction_remove` does not provide `.member`
+        # so we must get the member ourselves from the payload's `.user_id`.
+        member = guild.get_member(payload.user_id)
+        if member is None:
+            # Make sure the member still exists and is valid.
+            return
+
+        try:
+            # Finally, remove the role.
+            await member.remove_roles(role)
+            logger.info(f'Removed role {role} from {payload.member}')
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
 
 def setup(bot):
     bot.add_cog(Roles(bot)) # add the cog to the bot
